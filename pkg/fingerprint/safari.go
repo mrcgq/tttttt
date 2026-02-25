9fff33c7739a5e6197fb0914ed04cdda862a1bb2
mrcgq <pennymrs413@gmail.com>
2026-02-25 20:26:55 +0800

    Update safari.go

--- pkg/fingerprint/safari.go
+++ pkg/fingerprint/safari.go
@@ -1,66 +1,53 @@
 package fingerprint
- 
+
 import (
-	go-string">"github.com/user/tls-client/internal/h2"
-	utls go-string">"github.com/refraction-networking/utls"
+	"github.com/user/tls-client/internal/h2"
+	utls "github.com/refraction-networking/utls"
 )
- 
+
 func init() {
 	safariH2 := h2.SafariDefaultConfig()
- 
-	// Safari 17.4 - macOS
-	// Note: Using HelloIOS_Auto which is guaranteed to exist in utls v1.6.x.
-	// HelloSafari_Auto may be available in newer utls versions.
+
 	Register(&BrowserProfile{
-		Name:          go-string">"safari-go-number">17-mac",
-		Browser:       go-string">"safari",
-		Platform:      go-string">"macos",
-		Version:       go-string">"go-number">17.4.go-number">1",
+		Name:          "safari-17-mac",
+		Browser:       "safari",
+		Platform:      "macos",
+		Version:       "17.4.1",
 		ClientHelloID: utls.HelloIOS_Auto,
 		H2:            safariH2,
-		UserAgent:     go-string">"Mozilla/go-number">5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/go-number">605.1.go-number">15 (KHTML, like Gecko) Version/go-number">17.4.go-number">1 Safari/go-number">605.1.go-number">15",
+		UserAgent:     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15",
 	})
- 
-	// Safari 17.4 - iOS
+
 	Register(&BrowserProfile{
-		Name:          go-string">"safari-go-number">17-ios",
-		Browser:       go-string">"safari",
-		Platform:      go-string">"ios",
-		Version:       go-string">"go-number">17.4.go-number">1",
+		Name:          "safari-17-ios",
+		Browser:       "safari",
+		Platform:      "ios",
+		Version:       "17.4.1",
 		ClientHelloID: utls.HelloIOS_Auto,
 		H2:            safariH2,
-		UserAgent:     go-string">"Mozilla/go-number">5.0 (iPhone; CPU iPhone OS 17_4_1 like Mac OS X) AppleWebKit/go-number">605.1.go-number">15 (KHTML, like Gecko) Version/go-number">17.4.go-number">1 Mobile/go-number">15E148 Safari/go-number">604.1",
-		Tags:          []string{go-string">"mobile"},
+		UserAgent:     "Mozilla/5.0 (iPhone; CPU iPhone OS 17_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Mobile/15E148 Safari/604.1",
+		Tags:          []string{"mobile"},
 	})
- 
-	// Safari 17.5 - macOS (NEW)
+
 	Register(&BrowserProfile{
-		Name:          go-string">"safari-go-number">175-mac",
-		Browser:       go-string">"safari",
-		Platform:      go-string">"macos",
-		Version:       go-string">"go-number">17.5",
+		Name:          "safari-175-mac",
+		Browser:       "safari",
+		Platform:      "macos",
+		Version:       "17.5",
 		ClientHelloID: utls.HelloIOS_Auto,
 		H2:            safariH2,
-		UserAgent:     go-string">"Mozilla/go-number">5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/go-number">605.1.go-number">15 (KHTML, like Gecko) Version/go-number">17.5 Safari/go-number">605.1.go-number">15",
-		Tags:          []string{go-string">"latest"},
+		UserAgent:     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
+		Tags:          []string{"latest"},
 	})
- 
-	// Safari 17.5 - iOS (NEW)
+
 	Register(&BrowserProfile{
-		Name:          go-string">"safari-go-number">175-ios",
-		Browser:       go-string">"safari",
-		Platform:      go-string">"ios",
-		Version:       go-string">"go-number">17.5",
+		Name:          "safari-175-ios",
+		Browser:       "safari",
+		Platform:      "ios",
+		Version:       "17.5",
 		ClientHelloID: utls.HelloIOS_Auto,
 		H2:            safariH2,
-		UserAgent:     go-string">"Mozilla/go-number">5.0 (iPhone; CPU iPhone OS 17_5 like Mac OS X) AppleWebKit/go-number">605.1.go-number">15 (KHTML, like Gecko) Version/go-number">17.5 Mobile/go-number">15E148 Safari/go-number">604.1",
-		Tags:          []string{go-string">"latest", go-string">"mobile"},
+		UserAgent:     "Mozilla/5.0 (iPhone; CPU iPhone OS 17_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Mobile/15E148 Safari/604.1",
+		Tags:          []string{"latest", "mobile"},
 	})
 }
-
-
-
-
-
-
-
